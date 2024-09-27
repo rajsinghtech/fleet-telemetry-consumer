db7eb195cd3dfd27375deb649904679381069f79
Raj Singh <raj.singh@isovalent.com>
2024-09-27 02:13:29 -0400

    Add s3 bucket support

--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/rajsinghtech/fleet-telemetry-consumer
 go 1.22
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.31.0
-	github.com/aws/aws-sdk-go-v2/config v1.27.38
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.63.2
+	github.com/aws/aws-sdk-go v1.55.5
 	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/prometheus/client_golang v1.15.0
 	github.com/teslamotors/fleet-telemetry v0.3.2
@@ -13,24 +11,10 @@ require (
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.5 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.17.36 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.14 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.18 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.18 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.1 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.18 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.5 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.20 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.20 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.18 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.23.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.27.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.31.2 // indirect
-	github.com/aws/smithy-go v1.21.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "bytes"
-    "context"
     "encoding/json"
     "flag"
     "fmt"
@@ -10,13 +9,14 @@ import (
     "math"
     "net/http"
     "os"
-
+    "path/filepath"
     "strconv"
     "time"
 
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/config"
-    "github.com/aws/aws-sdk-go-v2/service/s3"
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/s3"
     "github.com/confluentinc/confluent-kafka-go/kafka"
     "github.com/prometheus/client_golang/prometheus"
     "github.com/prometheus/client_golang/prometheus/promhttp"
@@ -25,75 +25,99 @@ import (
     "google.golang.org/protobuf/proto"
 )
 
-// Define an S3 client and config
-var s3Client *s3.Client
-var s3Enabled bool
+type S3Config struct {
+    Endpoint  string `json:"endpoint"`
+    Bucket    string `json:"bucket"`
+    AccessKey string `json:"accessKey"`
+    SecretKey string `json:"secretKey"`
+    Region    string `json:"region"`
+}
+
+type KafkaConfig struct {
+    BootstrapServers string `json:"bootstrap.servers"`
+    GroupID          string `json:"group.id"`
+    AutoOffsetReset  string `json:"auto.offset.reset"`
+    Topic            string `json:"topic"`
+}
+
+var s3Svc *s3.S3
 var s3Bucket string
 
 func main() {
-    // Define a command-line flag for the configuration file path
     configFilePath := flag.String("config", "config.json", "Path to the JSON configuration file")
     flag.Parse()
 
-    // Read the configuration file
+    // Read and parse configuration
     configFile, err := os.ReadFile(*configFilePath)
     if err != nil {
         log.Fatalf("Failed to read configuration file: %s", err)
     }
 
-    // Parse the JSON configuration file
-    var configData map[string]interface{}
-    if err := json.Unmarshal(configFile, &configData); err != nil {
+    var configMap map[string]interface{}
+    if err := json.Unmarshal(configFile, &configMap); err != nil {
         log.Fatalf("Failed to parse configuration file: %s", err)
     }
 
-    // Load Kafka configuration
-    kafkaConfig, ok := configData["kafka"].(map[string]interface{})
-    if !ok {
-        log.Fatalf("Kafka configuration missing or incorrect format")
-    }
-
-    // Convert the map to kafka.ConfigMap
-    kafkaCfgMap := kafka.ConfigMap{}
-    for key, value := range kafkaConfig {
-        kafkaCfgMap[key] = value
-    }
-
-    // Check for AWS configuration
-    awsConfig, awsOk := configData["aws"].(map[string]interface{})
-    if awsOk {
-        if bucket, exists := awsConfig["s3Bucket"].(string); exists && bucket != "" {
-            s3Bucket = bucket
-            initS3Client() // Initialize AWS S3 client if config exists
-        } else {
-            log.Println("S3 bucket is not configured correctly. S3 uploads will be disabled.")
-            s3Enabled = false
+    // Kafka configuration
+    kafkaConfigMap := configMap["kafka"].(map[string]interface{})
+    kafkaConfig := kafka.ConfigMap{}
+    for key, value := range kafkaConfigMap {
+        if key != "topic" {  // Exclude "topic" from KafkaConfigMap
+            kafkaConfig[key] = value
         }
-    } else {
-        log.Println("AWS configuration missing. S3 uploads are not enabled.")
-        s3Enabled = false
     }
 
-    // Create the Kafka consumer
-    consumer, err := kafka.NewConsumer(&kafkaCfgMap)
+    // Get the topic from the config
+    topic, ok := kafkaConfigMap["topic"].(string)
+    if !ok || topic == "" {
+        log.Fatal("Kafka topic not specified in the configuration.")
+    }
+
+    // S3 configuration and connection test
+    s3Config, s3Enabled := configMap["aws"].(map[string]interface{})
+    if s3Enabled {
+        log.Println("S3 is enabled, testing S3 connection...")
+
+        s3ConfigStruct := parseS3Config(s3Config)
+        s3Svc = configureS3(s3ConfigStruct)
+        s3Bucket = s3ConfigStruct.Bucket
+
+        if s3Svc == nil {
+            log.Fatal("S3 configuration is invalid. Exiting.")
+        }
+
+        if !testS3Connection(s3Svc) {
+            log.Fatal("S3 connection failed. Exiting.")
+        }
+
+        log.Println("S3 connection test successful.")
+    } else {
+        log.Println("S3 configuration not found. S3 upload is disabled.")
+    }
+
+    // Create Kafka consumer
+    consumer, err := kafka.NewConsumer(&kafkaConfig)
     if err != nil {
         log.Fatalf("Failed to create consumer: %s", err)
     }
     defer consumer.Close()
 
-    // Get topics
+    // Get topics and log them in one line
     metadata, err := consumer.GetMetadata(nil, true, 10000)
     if err != nil {
         log.Fatalf("Failed to get metadata: %s", err)
     }
 
-    fmt.Println("Available topics:")
-    for topic := range metadata.Topics {
-        fmt.Println(topic)
+    topics := make([]string, 0, len(metadata.Topics))
+    for t := range metadata.Topics {
+        topics = append(topics, t)
     }
 
-    // Subscribe to the topic
-    err = consumer.Subscribe("tesla_V", nil)
+    log.Printf("Available topics: %s", topics)
+    log.Printf("Selected topic: %s", topic)
+
+    // Subscribe to the Kafka topic
+    err = consumer.Subscribe(topic, nil)
     if err != nil {
         log.Fatalf("Failed to subscribe to topic: %s", err)
     }
@@ -106,8 +130,6 @@ func main() {
         },
         []string{"field", "vin"},
     )
-
-    // Register the metric
     prometheus.MustRegister(vehicleDataGauge)
 
     // Start HTTP server for Prometheus metrics
@@ -139,15 +161,8 @@ func main() {
             log.Printf("Failed to marshal vehicleData to JSON: %v\n", err)
             continue
         }
-        fmt.Println(string(vehicleDataJSON))
 
-        // If S3 is enabled, upload to S3
-        if s3Enabled {
-            err = uploadToS3(vehicleData.Vin, vehicleDataJSON)
-            if err != nil {
-                log.Printf("Failed to upload data to S3: %v\n", err)
-            }
-        }
+        fmt.Println(string(vehicleDataJSON))
 
         // Process each Datum in the Payload
         for _, datum := range vehicleData.Data {
@@ -155,123 +170,163 @@ func main() {
             value := datum.Value
             processValue(fieldName, value, vehicleDataGauge, vehicleData.Vin)
         }
+
+        // Upload to S3 (if enabled)
+        if s3Svc != nil {
+            err = uploadToS3(s3Svc, s3Bucket, vehicleDataJSON)
+            if err != nil {
+                log.Printf("Failed to upload vehicle data to S3: %v", err)
+            }
+        }
     }
 }
 
-// initS3Client initializes the S3 client with AWS SDK
-func initS3Client() {
-    cfg, err := config.LoadDefaultConfig(context.TODO())
-    if err != nil {
-        log.Fatalf("Unable to load AWS SDK config: %v", err)
+// (Rest of the functions stay the same: parseS3Config, configureS3, testS3Connection, uploadToS3, processValue, handle*Value functions)
+
+// Parse the S3 configuration from the map
+func parseS3Config(s3Config map[string]interface{}) *S3Config {
+    return &S3Config{
+        Endpoint:  s3Config["endpoint"].(string),
+        Bucket:    s3Config["bucket"].(string),
+        AccessKey: s3Config["accessKey"].(string),
+        SecretKey: s3Config["secretKey"].(string),
+        Region:    s3Config["region"].(string),
     }
-    s3Client = s3.NewFromConfig(cfg)
-    s3Enabled = true
-    log.Println("S3 uploads are enabled.")
 }
 
-// uploadToS3 uploads vehicle data to the specified S3 bucket
-func uploadToS3(vin string, data []byte) error {
-    key := fmt.Sprintf("vehicle-data/%s/%s.json", vin, time.Now().Format("2006-01-02T15-04-05"))
-
-    input := &s3.PutObjectInput{
-        Bucket: aws.String(s3Bucket),
-        Key:    aws.String(key),
-        Body:   bytes.NewReader(data),
-        ContentType: aws.String("application/json"),
+// Configure S3 client
+func configureS3(s3Config *S3Config) *s3.S3 {
+    if s3Config == nil || s3Config.Endpoint == "" || s3Config.Bucket == "" ||
+        s3Config.AccessKey == "" || s3Config.SecretKey == "" || s3Config.Region == "" {
+        log.Println("S3 configuration is incomplete.")
+        return nil
     }
 
-    _, err := s3Client.PutObject(context.TODO(), input)
+    sess, err := session.NewSession(&aws.Config{
+        Region:      aws.String(s3Config.Region),
+        Credentials: credentials.NewStaticCredentials(s3Config.AccessKey, s3Config.SecretKey, ""),
+        Endpoint:    aws.String(s3Config.Endpoint),
+    })
     if err != nil {
-        return fmt.Errorf("unable to upload data to S3: %w", err)
+        log.Printf("Failed to create AWS session: %v", err)
+        return nil
     }
 
-    log.Printf("Successfully uploaded data to S3: s3://%s/%s", s3Bucket, key)
-    return nil
+    return s3.New(sess)
+}
+
+// Test the S3 connection by making a simple ListBuckets request
+func testS3Connection(s3Svc *s3.S3) bool {
+    _, err := s3Svc.ListBuckets(&s3.ListBucketsInput{})
+    if err != nil {
+        log.Printf("Failed to connect to S3: %v", err)
+        return false
+    }
+    return true
+}
+// Example upload function (not used in connection test)
+func uploadToS3(s3Svc *s3.S3, bucket string, data []byte) error {
+	timestamp := time.Now().Format("2006/01/02/15-04-05")
+	key := filepath.Join(timestamp, "data.json")
+
+	input := &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(data),
+	}
+
+	_, err := s3Svc.PutObject(input)
+	if err != nil {
+		log.Printf("Failed to upload file to S3: %v", err)
+		return err
+	}
+	log.Printf("Successfully uploaded data to S3 at key: %s", key)
+	return nil
 }
 
 func processValue(fieldName string, value *protos.Value, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    // Process each value type
-    switch v := value.Value.(type) {
-    case *protos.Value_DoubleValue:
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(v.DoubleValue)
-    case *protos.Value_FloatValue:
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.FloatValue))
-    case *protos.Value_IntValue:
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.IntValue))
-    case *protos.Value_LongValue:
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.LongValue))
-    case *protos.Value_BooleanValue:
-        handleBooleanValue(v.BooleanValue, fieldName, vehicleDataGauge, vin)
-    case *protos.Value_StringValue:
-        handleStringValue(v.StringValue, fieldName, vehicleDataGauge, vin)
-    case *protos.Value_Invalid:
-        log.Printf("Invalid value received for field %s, setting as NaN", fieldName)
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
-    case *protos.Value_LocationValue:
-        handleLocationValue(v.LocationValue, fieldName, vehicleDataGauge, vin)
-    case *protos.Value_DoorValue:
-        handleDoorValues(v.DoorValue, vehicleDataGauge, vin)
-    case *protos.Value_TimeValue:
-        handleTimeValue(v.TimeValue, fieldName, vehicleDataGauge, vin)
-    default:
-        log.Printf("Unhandled value type for field %s", fieldName)
-    }
+	// Process each value type
+	switch v := value.Value.(type) {
+	case *protos.Value_DoubleValue:
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(v.DoubleValue)
+	case *protos.Value_FloatValue:
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.FloatValue))
+	case *protos.Value_IntValue:
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.IntValue))
+	case *protos.Value_LongValue:
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(float64(v.LongValue))
+	case *protos.Value_BooleanValue:
+		handleBooleanValue(v.BooleanValue, fieldName, vehicleDataGauge, vin)
+	case *protos.Value_StringValue:
+		handleStringValue(v.StringValue, fieldName, vehicleDataGauge, vin)
+	case *protos.Value_Invalid:
+		log.Printf("Invalid value received for field %s, setting as NaN", fieldName)
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
+	case *protos.Value_LocationValue:
+		handleLocationValue(v.LocationValue, fieldName, vehicleDataGauge, vin)
+	case *protos.Value_DoorValue:
+		handleDoorValues(v.DoorValue, vehicleDataGauge, vin)
+	case *protos.Value_TimeValue:
+		handleTimeValue(v.TimeValue, fieldName, vehicleDataGauge, vin)
+	default:
+		log.Printf("Unhandled value type for field %s", fieldName)
+	}
 }
 
 func handleBooleanValue(booleanValue bool, fieldName string, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    var numericValue float64
-    if booleanValue {
-        numericValue = 1.0
-    } else {
-        numericValue = 0.0
-    }
-    vehicleDataGauge.WithLabelValues(fieldName, vin).Set(numericValue)
+	var numericValue float64
+	if booleanValue {
+		numericValue = 1.0
+	} else {
+		numericValue = 0.0
+	}
+	vehicleDataGauge.WithLabelValues(fieldName, vin).Set(numericValue)
 }
 
 func handleStringValue(stringValue, fieldName string, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    // Handle invalid string values more gracefully
-    if stringValue == "<invalid>" || stringValue == "\u003cinvalid\u003e" {
-        log.Printf("Invalid string value received for field %s, setting as NaN", fieldName)
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
-        return
-    }
+	// Handle invalid string values more gracefully
+	if stringValue == "<invalid>" || stringValue == "\u003cinvalid\u003e" {
+		log.Printf("Invalid string value received for field %s, setting as NaN", fieldName)
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
+		return
+	}
 
-    // Try to parse the string value as a float64
-    floatVal, err := strconv.ParseFloat(stringValue, 64)
-    if err == nil {
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(floatVal)
-    } else {
-        log.Printf("Non-numeric string value received for field %s: %s, setting as NaN", fieldName, stringValue)
-        vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
-    }
+	// Try to parse the string value as a float64
+	floatVal, err := strconv.ParseFloat(stringValue, 64)
+	if err == nil {
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(floatVal)
+	} else {
+		log.Printf("Non-numeric string value received for field %s: %s, setting as NaN", fieldName, stringValue)
+		vehicleDataGauge.WithLabelValues(fieldName, vin).Set(math.NaN())
+	}
 }
 
 func handleLocationValue(locationValue *protos.LocationValue, fieldName string, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    vehicleDataGauge.WithLabelValues("Latitude", vin).Set(locationValue.Latitude)
-    vehicleDataGauge.WithLabelValues("Longitude", vin).Set(locationValue.Longitude)
+	vehicleDataGauge.WithLabelValues("Latitude", vin).Set(locationValue.Latitude)
+	vehicleDataGauge.WithLabelValues("Longitude", vin).Set(locationValue.Longitude)
 }
 
 func handleDoorValues(doors *protos.Doors, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    doorFields := map[string]bool{
-        "DriverFrontDoor":    doors.DriverFront,
-        "PassengerFrontDoor": doors.PassengerFront,
-        "DriverRearDoor":     doors.DriverRear,
-        "PassengerRearDoor":  doors.PassengerRear,
-        "TrunkFront":         doors.TrunkFront,
-        "TrunkRear":          doors.TrunkRear,
-    }
-    for doorName, state := range doorFields {
-        var numericValue float64
-        if state {
-            numericValue = 1.0
-        } else {
-            numericValue = 0.0
-        }
-        vehicleDataGauge.WithLabelValues(doorName, vin).Set(numericValue)
-    }
+	doorFields := map[string]bool{
+		"DriverFrontDoor":    doors.DriverFront,
+		"PassengerFrontDoor": doors.PassengerFront,
+		"DriverRearDoor":     doors.DriverRear,
+		"PassengerRearDoor":  doors.PassengerRear,
+		"TrunkFront":         doors.TrunkFront,
+		"TrunkRear":          doors.TrunkRear,
+	}
+	for doorName, state := range doorFields {
+		var numericValue float64
+		if state {
+			numericValue = 1.0
+		} else {
+			numericValue = 0.0
+		}
+		vehicleDataGauge.WithLabelValues(doorName, vin).Set(numericValue)
+	}
 }
 
 func handleTimeValue(timeValue *protos.Time, fieldName string, vehicleDataGauge *prometheus.GaugeVec, vin string) {
-    totalSeconds := float64(timeValue.Hour*3600 + timeValue.Minute*60 + timeValue.Second)
-    vehicleDataGauge.WithLabelValues(fieldName, vin).Set(totalSeconds)
+	totalSeconds := float64(timeValue.Hour*3600 + timeValue.Minute*60 + timeValue.Second)
+	vehicleDataGauge.WithLabelValues(fieldName, vin).Set(totalSeconds)
 }
